3b198e59d55f0e40bb38b5d111e934442ff56c65
Sivaji gadidala <sivajigsivajig703@gmail.com>
2023-11-06 12:44:51 +0530

    Layouts and Components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>2023. All rights reserved.</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
