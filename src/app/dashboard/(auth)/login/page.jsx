d31a28c5b689c0629c9b435d43b995df9641a8e9
Sivaji gadidala <sivajigsivajig703@gmail.com>
2023-11-09 14:20:39 +0530

    credentials implementing

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -4,8 +4,43 @@ import styles from './page.module.css'
 import { signIn } from 'next-auth/react'
 
 const Login = () => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    console.log(email, password);
+
+    signIn("credentials", {
+      email,
+      password,
+    });
+  };
+
+
   return (
     <div className={styles.container}>
+       
+       <form onSubmit={handleSubmit} className={styles.form}>
+       
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+        {/* {error && "Something went wrong!"} */}
+        
+      </form>
+
         <button onClick={()=>signIn("google")}>Login with Google</button>
     </div>
   )
