f5497d7698897bd79378b0f059f767ddadd94bfd
Sivaji gadidala <sivajigsivajig703@gmail.com>
2023-11-10 13:33:52 +0530

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Image from "next/image";
-import styles from './page.module.css'
-import Button from '@components/Button/Button'
+import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+import Button from '@components/Button/Button';
 
 export const metadata = {
   title: 'Contact Us - Blog App SJ',
   description: 'Get in touch with us for any questions or feedback about our blog app.',
-}
-
+};
 
 const Contact = () => {
   return (
@@ -15,29 +14,23 @@ const Contact = () => {
       <h1 className={styles.title}>Let's Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
-        <Image
-          src='/contact.png'
-          fill={true}
-          alt=""
-          className={styles.image}
-        />
+          <Image
+            src='/contact.png'
+            alt=""
+            layout='fill'
+            className={styles.image}
+          />
         </div>
 
-          <form className={styles.form}>
-              <input type="text" placeholder='name' className={styles.input} id="" />
-              <input type="email" placeholder='email' className={styles.input} id="" />
-              <textarea placeholder='message'
-               className={styles.textArea} cols="30" rows="10">
-
-              </textarea>
-         <Button url="/#" text="Send"/>
-         
-
-          </form>
+        <form className={styles.form}>
+          <input type="text" placeholder='name' className={styles.input} id="name" />
+          <input type="email" placeholder='email' className={styles.input} id="email" />
+          <textarea placeholder='message' className={styles.textArea} cols="30" rows="10" id="message"></textarea>
+          <Button url="/#" text="Send" />
+        </form>
       </div>
-    
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
